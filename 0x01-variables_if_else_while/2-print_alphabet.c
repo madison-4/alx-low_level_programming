dc4d2799ad26d8602a2eea5d248afb96088ed917
nox <samingosi97@gmail.com>
2023-07-14 16:37:45 -0700

    print last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: no return
+ */
+void main(void)
+{
+	int character = 97;
+
+	while (character <= 122)
+		putchar(character);
+	putchar(10);
+}
