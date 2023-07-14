8087bce674a4ac35807cb5a509853282147b5b8b
nox <samingosi97@gmail.com>
2023-07-14 16:39:18 -0700

    make main return int

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 /**
  * main - print alphabet in lowercase
- * Return: no return
+ * Return: 0 on success
  */
-void main(void)
+int main(void)
 {
 	int character = 97;
 
 	while (character <= 122)
 		putchar(character);
 	putchar(10);
+	return (0);
 }
