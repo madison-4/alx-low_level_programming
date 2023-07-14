7a99cb6fb358e3f4e7f060a54ed39165d8f3a3d5
nox <samingosi97@gmail.com>
2023-07-13 18:35:00 -0700

    print a line with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print a line
+ * Return: 0 on success
+ * no argum,ents
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
