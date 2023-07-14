ab45b27ce6310896ae6cc8fdcc4670a1b69192a6
nox <samingosi97@gmail.com>
2023-07-13 18:42:57 -0700

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - hello using printf
+ * Return: 0 on success
+ * no arguments
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
