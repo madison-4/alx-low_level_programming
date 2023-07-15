6a3c1b62e42fcc5062aa784c177da167bebd060e
nox <samingosi97@gmail.com>
2023-07-15 03:30:17 -0700

    print single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - print all single digit numbers
+ * no arguents
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while(digit <= 57)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar(36);
+		}
+		digit++;
+	}
+	return (0);
+}
