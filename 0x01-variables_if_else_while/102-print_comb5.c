d60b3a21b850dd3a3e1764ff3e6bdcad4816beee
nox <samingosi97@gmail.com>
2023-07-15 11:35:59 -0700

    print double doube

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - print double dogit numbers without repetition
+ * no arguments
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int firstnumber = 0, secondnumber;
+
+	while (firstnumber <= 98)
+	{
+		secondnumber = firstnumber + 1;
+		while (secondnumber <= 99)
+		{
+			putchar((firstnumber / 10) + 48);
+			putchar((firstnumber % 10) + 48);
+			putchar(32);
+			putchar((secondnumber / 10) + 48);
+			putchar((secondnumber % 10) + 48);
+			if ((firstnumber != 98) && (secondnumber != 99)
+			    putchar(44);
+			    putchar(32);
+		}
+		firstnumber++;
+	}
+	putchar(10);
+	return (0);
+}
