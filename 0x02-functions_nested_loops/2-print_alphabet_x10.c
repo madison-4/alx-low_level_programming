5b9f6c9b468a14fc1a61d52f3116584925a817f4
nox <samingosi97@gmail.com>
2023-07-18 10:55:30 -0700

    print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Return: no return
+ * no arguments
+ */
+void print_alphabet_x10(void)
+{
+	int alphcount, count = 0;
+
+	while (count <= 9)
+	{
+		alphcount = 97;
+		while (alphcount <= 122)
+		{
+			_putchar(alphcount);
+		}
+		count++;
+	}
+}
