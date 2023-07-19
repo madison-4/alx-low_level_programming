4bdc6c7b04b23415a4e96fd441ea3daaaa1669ad
nox <samingosi97@gmail.com>
2023-07-18 18:24:00 -0700

    use last digt value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	_putchar((n % 10) + 48);
-	if (n < 0)
-		return ((n % 10) * -1);
-	else
-		return (n % 10);
+	int lastdigit = n % 10;
+
+	if (lastdigit < 0)
+		lastdigit *= -1;
+	_putchar(lastdigit + 48);
+	return (lastdigit);
 }
