0aeedf777061d85f6ea69e665d89a6ee1a8e3302
nox <samingosi97@gmail.com>
2023-07-18 19:02:03 -0700

    rint n time tale

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times tale
+ * @n: number of tables to print
+ * no return
+ */
+void times_table(int n)
+{
+	int row, column, prod;
+
+	if ((n < 0) || (n > 15))
+		return;
+	for (row = 0; row <= n; row++)
+	{
+		_putchar('0');
+		for (column = 1; column < n; column++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = row * column;
+			if (prod < 10)
+				_putchar(32);
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
