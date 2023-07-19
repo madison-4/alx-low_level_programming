fcea207b1916c80b20e6253f68a8205222c63b5c
nox <samingosi97@gmail.com>
2023-07-18 19:19:03 -0700

    print natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - sum multiple of 3 and 5 below 1024
+ * no arguments
+ * Return: 0 on success nonzero otherwise
+ */
+int main(void)
+{
+	int multiple, sum = 0, iterator;
+
+	for (iterator = 3; iterator < 1024; iterator++)
+	{
+		if ((iterator % 3) == 0 || (iterator % 5) == 0)
+			sum += iterator;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
