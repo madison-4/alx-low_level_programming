88676952b352d013df1076b61a29d9f0b662e7bd
nox <samingosi97@gmail.com>
2023-07-20 16:38:38 -0700

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit  check if given argument's a digit
+ * @c: given argument
+ * Return:  if it is, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply 2 integers
+ * @a: first integr
+ * @b: secod integer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
