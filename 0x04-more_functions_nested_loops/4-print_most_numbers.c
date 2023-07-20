d414d84837de01b74aefaad93763b9dcc18e409b
nox <samingosi97@gmail.com>
2023-07-20 16:44:36 -0700

    print digits except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 to 9 followed  y newline
+ * except 2 and 4
+ * no arguments
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if ((i == 50) || (i == 52))
+			continue;
+		_putchar(i);
+	}
+	_putchar(10);
+}
