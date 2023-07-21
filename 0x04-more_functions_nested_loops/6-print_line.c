b2099efcfe6e5738e4da300b018759d82af2d78c
nox <samingosi97@gmail.com>
2023-07-20 17:28:15 -0700

    print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,20 @@
 #include "main.h"
+/**
+ * print_line - print a line then newline
+ * @n: length of the line in unescores
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+}
