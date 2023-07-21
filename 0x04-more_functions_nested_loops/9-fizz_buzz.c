8a3523900f35dc4adf8a3dcbd08a6abbb0806add
nox <samingosi97@gmail.com>
2023-07-20 18:44:37 -0700

    fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - print fizzbuzz test
+ * Return: 0 on succes
+ * no arguments
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz ");
+		else if	((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
