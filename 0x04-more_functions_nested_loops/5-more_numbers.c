f88b849fe5c7aa5c1d1db45f822d3032a9ca19a3
nox <samingosi97@gmail.com>
2023-07-20 17:20:38 -0700

    print0 to 14 ten ties

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,18 @@
 void more_numbers(void)
 {
 
-	int i, tensdigit, onesdigit;
+	int i, tensdigit, onesdigit, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		tensdigit = i / 10;
-		onesdigit = i % 10;
-		if (tensdigit != 0)
-			_putchar(tensdigit + 48);
-		_putchar(onesdigit + 48);
+		for (j = 0; j <= 14; j++)
+		{
+			tensdigit = j / 10;
+			onesdigit = j % 10;
+			if (tensdigit != 0)
+				_putchar(tensdigit + 48);
+			_putchar(onesdigit + 48);
+		}
 		_putchar(10);
 	}
 }
