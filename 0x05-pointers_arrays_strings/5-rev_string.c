5ea64d6f36256081c566daf1d7266a2a0cff9afc
nox <samingosi97@gmail.com>
2023-07-24 15:26:08 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strlen - get ength excusing null byte
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	return (length);
+}
+/**
+ * rev_string - revrse a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int length = _strlen(s);
+	int i;
+
+	for (i = length; i < (length / 2); i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
