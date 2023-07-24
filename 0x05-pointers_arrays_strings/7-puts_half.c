6f9f22149cdaf7d8dce17747c3b1d9c8b2e5d0fb
nox <samingosi97@gmail.com>
2023-07-24 16:51:45 -0700

    print sceond half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strlen - gets the length of teh string
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int length = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * puts_half - print first half of string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int length, i;
+
+	length = _strlen(str);
+	for (i = (length / 2) + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
