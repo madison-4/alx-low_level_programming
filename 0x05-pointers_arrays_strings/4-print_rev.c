fe58ac40ed65c80f5a7954e5cbbf3972e7f6bed0
nox <samingosi97@gmail.com>
2023-07-24 14:57:15 -0700

    clone puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strlen - get string length
+ * @str: string
+ * Return: length os string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ * no return
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
