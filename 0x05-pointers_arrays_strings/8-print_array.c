0f547bf23a688d5085a6e4d9b6081c00a75ecd9d
nox <samingosi97@gmail.com>
2023-07-24 18:31:11 -0700

    print ints

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - print an array of ints, n size
+ * @a: the array
+ * @n: the number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n)
+			printf(", ");
+	}
+	printf("\n");
+}
