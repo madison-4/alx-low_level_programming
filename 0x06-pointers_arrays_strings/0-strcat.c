9c1ec5efc90edae6a3a687b5ac08b05ca50053d9
nox <samingosi97@gmail.com>
2023-07-31 13:44:54 -0700

    add all files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -25,7 +25,7 @@ char *_strcat(char *dest, char *src)
 {
 	int iterator = 0, start = _strlen(dest), srclen = _strlen(src), i = 1;
 
-	for (iterator = 0; iterator <= srclen; iterator++)
+	for (iterator = 0; iterator <= (srclen + 2); iterator++)
 	{
 		dest[start + i] = src[iterator];
 		i++;
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 C excercises
 ## 0x02-functions_nested_loops
 looks at excercises on loops and nested loops
-
+## Pointers in C
 # Author
 mdison-4/nox
