3f8a894744cd545014805e5fc35dde3af25adffb
nox <samingosi97@gmail.com>
2023-07-31 19:20:59 -0700

    find a c haracter in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locate a charactr in a string
+ * @s: string to be looked into
+ * @c: character to find
+ * Return: first occurrence if caracter or NULL if it's not found
+ */
+char *_strchr(char *s, char c)
+{
+	int iter;
+
+	for (iter = 0; s[iter] != '\0'; iter++)
+	{
+		if (s[iter] == c)
+		{
+			return (s + iter);
+			break;
+		}
+	}
+	return (NULL);
+}
