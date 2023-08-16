6dd2229f6d42ee6a503fb2f4cc257761cf205fdc
nox <samingosi97@gmail.com>
2023-08-16 06:18:26 -0700

    use recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,19 +4,11 @@
  * @n: number to be printed
  * Return: no return
  */
-void print_number(int n)
+void print_number(long int n)
 {
-	int quotient, remainder;
-
 	if (n < 0)
 		n *= -1;
-	quotient = n / 10;
-	remainder = n % 10;
-	if (quotient > remainder)
+	if (n / 10)
 		print_number(n);
-	else
-	{
-		_putchar(remainder + 48);
-		return;
-	}
+	_putchar((n % 10) + 48);
 }
