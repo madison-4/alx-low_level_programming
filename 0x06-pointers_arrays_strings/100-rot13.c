4ddf227c34870fc6933707a083ba9bead74a6f69
nox <samingosi97@gmail.com>
2023-08-20 04:48:27 -0700

    use isalpha

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,5 @@
 #include "main.h"
-/**
- * _isalpha - checks if a char is an alphabetical char
- * @c: char to check
- * Return: 1 if its is, 0 otherwise
- */
-int _isalpha(char c)
-{
-	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
-		return (1);
-	return (0);
-}
+#include "main.h"
 /**
  * rot13 - encode a string by moving its chars by 13 chars.
  * @s: string to be encoded
@@ -17,16 +7,19 @@ int _isalpha(char c)
  */
 char *rot13(char *s)
 {
-	int i = 0;
+	int i = 0, arriter;
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (s[i])
 	{
-		while (_isalpha(s[i]))
+		for (arriter = 0; arriter < 52; arriter++)
 		{
-			if (((s[i] >= 78) && (s[i] <= 90)) || ((s[i] >= 110) && (s[i] <= 122)))
-				s[i] = (s[i] - 13);
-			s[i] = (s[i] + 13);
-			i++;
+			if (s[i] == alph[arriter])
+			{
+				s[i] = rot[arriter];
+				break;
+			}
 		}
 		i++;
 	}
