7de1f4e1a254aa88d90e8df327fc531013ffc35e
nox <samingosi97@gmail.com>
2023-08-19 20:08:29 -0700

    manke strncpy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,8 +11,8 @@ void reverse_array(int *a, int n)
 
 	for (iter = 0; iter <= count; iter++)
 	{
-		a[n - 1] = temp;
-		a[n - 1] = a[iter];
+		a[n] = temp;
+		a[n] = a[iter];
 		a[iter] = temp;
 		n--;
 	}
