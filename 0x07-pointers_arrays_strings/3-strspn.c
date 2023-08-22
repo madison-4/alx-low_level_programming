ebba39dc64266209dc5ce08c7169c5d863e4f61f
nox <samingosi97@gmail.com>
2023-08-22 09:00:35 -0700

    use strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,38 @@
 #include "main.h"
 /**
- * _strspn - clone strspn function in C
- * @s: initial segment
- * @accept: string compared tpo
- * Return: number of bytes present
+ * _strchr - locate a charactr in a string
+ * @s: string to be looked into
+ * @c: character to find
+ * Return: first occurrence if caracter or NULL if it's not found
+ */
+char *_strchr(char *s, char c)
+{
+	int iter;
+
+	for (iter = 0; s[iter] != '\0'; iter++)
+	{
+		if (s[iter] == c)
+		{
+			return (s + iter);
+		}
+	}
+	if (c == '\0')
+		return (s + iter);
+	return (NULL);
+}
+/**
+ * _strspn - find the nunber of matching characters from the beginning
+ * @s: stribng to find
+ * @accept: from where to find the match
+ * Return: number of match
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0, iter = 0, begin = 0;
+	unsigned int len = 0;
 
-	while (s[begin])
-	{
-		for (iter = 0; accept[iter] ; iter++)
-		{
-			if ((s[begin] == accept[iter]))
-			{
-				count++;
-				break;
-			}
-			else if (s[begin] == accept[iter] + 32)
-			{
-				count++;
-				break;
-			}
-			else if (s[begin] == accept[iter] + 32)
-			{
-				count++;
-				break;
-			}
-		}
-		begin++;
-	}
-	return (count);
+	if ((s == NULL) || (accept == NULL))
+		return (len);
+	while (*s && _strchr(accept, *s++))
+		len++;
+	return (len);
 }
