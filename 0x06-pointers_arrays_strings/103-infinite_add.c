9b9810fb240f76df521d6c4f9e66e6205288cf42
nox <samingosi97@gmail.com>
2023-08-22 04:26:00 -0700

    cast to chars then add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _strlen - get length of a string
+ * @str: char pointer
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+/**
+ * infinite_add - adds two integers stored as strings
+ * @n1: first integer
+ * @n2: second integer
+ * @r: buffer to store result
+ * @size_r: size of r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int iter, carry = 0, n1len = _strlen(n1), n2len = _strlen(n2);
+	int fill = size_r;
+
+	while ((n1len >= 0) || (n2len >= 0))
+	{
+		if (!(n1[n1len]))
+			n1[n1len] = 0;
+		if (!(n2[n2len]))
+			n2[n2len] = 0;
+		iter = ((((int)n1[n1len]) + ((int)n2[n2len])) + carry) % 10;
+		carry = ((((int)n1[n1len]) + ((int)n2[n2len])) + carry) / 10;
+		if (fill <= 0)
+			return (0);
+		r[fill] = (char) iter;
+		n1len--;
+		n2len--;
+		fill--;
+	}
+	return (r);
+}
