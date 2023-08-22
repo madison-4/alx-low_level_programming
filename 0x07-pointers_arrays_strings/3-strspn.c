1d8f92abb99260929dfbe425350da26b54b85f9e
nox <samingosi97@gmail.com>
2023-08-22 06:27:51 -0700

    add chars

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - clone strspn function in C
+ * @s: initial segment
+ * @accept: string compared tpo
+ * Return: number of bytes present
+ */
+int _strspn(char *s, char *accept)
+{
+	int count = 0, iter = 0, begin = 0;
+
+	while (s[begin])
+	{
+		for (iter = 0; accept[iter] ; iter++)
+		{
+			if (s[begin] == accept[iter])
+				count++;
+		}
+		begin++;
+	}
+	return (count);
+}
