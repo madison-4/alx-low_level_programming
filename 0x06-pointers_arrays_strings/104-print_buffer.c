bf2f0e944bc5616eb21b4a14006d638f33d384c5
nox <samingosi97@gmail.com>
2023-08-26 22:01:13 -0700

    still to account for zeros at the end

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,11 +13,14 @@ void print_buffer(char *b, int size)
 	for (iter = 0; iter < size; iter += 10)
 	{
 		printf("%08x: ", iter);
+		for (i = 0; i < 10 && ((i + iter) <= size); i += 2)
+		{
+			printf("%02x%02x ", b[i + iter], b[i + 1 + iter]);
+		}
 		for (i = 0; i < 10 && ((i + iter) < size); i++)
 		{
-			printf("%02x ", b[i + iter]);
 			if (isprint(b[i + iter]))
-				putchar(b[i + iter]);
+			    putchar(b[i + iter]);
 			else
 				putchar('.');
 		}
