d3284f5defdd5437fdbbad832c53acd9183e470d
nox <samingosi97@gmail.com>
2023-08-26 23:46:29 -0700

    change the value of a string and set it

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a char pointer
+ * @s1: The double pointer holding teh address to set to
+ * @to: pointer to be set to
+ * Return: no return
+ */
+void set_string(char **s1, char *to)
+{
+	*s1 = to;
+}
