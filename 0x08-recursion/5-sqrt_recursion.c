6f67211d96e7fae162ef6c4557543647e33022de
Enox <anzimboingosi@gmail.com>
2023-08-31 00:00:34 +0300

    use a helper function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * findroot - finds the root from a guess
+ * @number: number to find root of
+ * @guess: initial guess, starts at 1
+ * Return: squareroot of number
+ */
 int findroot(int number, int guess)
 {
 	if ((guess * guess) > number)
