7a3f12c4b5ce68ad033e85b8353d5dcd688ffab3
Enox <anzimboingosi@gmail.com>
2023-09-05 04:22:53 +0300

    clone strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - find length of a string
+ * @str: string to find length of
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int len = 0, iter;
+
+	for (iter = 0; str[iter]; iter++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strdup - clone strdup from stdlib
+ * @str: string to be copied
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int fill = _strlen(str), iter;
+
+	if ((fill == 0) || (str == NULL))
+		return (NULL);
+	new = malloc(sizeof(char) * fill);
+	if (new == NULL)
+		return (NULL);
+	for (iter = 0; iter < fill; iter++)
+	{
+		new[iter] = str[iter];
+	}
+	return (new);
+}
