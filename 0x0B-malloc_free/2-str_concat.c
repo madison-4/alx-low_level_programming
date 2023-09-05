9db2921dd40496d1f656b08ee9a01cb9fdaa1fa8
nox <samingosi97@gmail.com>
2023-09-05 12:37:50 -0700

    copy s1 then s2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
- * _strcpy - copy a string
- * @src: address o source string
- * @dest: destination address
- * Return: pointer to new address
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
  * _strlen - get the length of a string
  * @s: string
  * Return: length of the string
@@ -32,28 +15,7 @@ int _strlen(char *s)
 	return (length);
 }
 /**
- * _strcat - concatentaes two strings overwriting the terinating null byte
- * and adding it to teh end
- * @dest: string to append to
- * @src: string to append
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int iter = 0, lendest = _strlen(dest);
-
-	while (src[iter] != '\0')
-	{
-		dest[lendest] = src[iter];
-		iter++;
-		lendest++;
-	}
-	dest[lendest + 1] = '\0';
-	return (dest);
-}
-/**
  * str_concat - concatenate two srings in a new place in memory
- *
  * @s1: string 1
  * @s2: string 2
  * Return: newly allocated string pointer
@@ -61,24 +23,22 @@ char *_strcat(char *dest, char *src)
 char *str_concat(char *s1, char *s2)
 {
 	int len1 = _strlen(s1), len2 = _strlen(s2), size = len1 + len2 + 1;
-	int iter, it = 0;
+	int iter = 0, it = 0;
 	char *new;
 
-	new = malloc((sizeof(char) * size));
-	if (new == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
-	for (iter = 0; s1[iter]; iter++)
-		new[iter] = s1[iter];
 	if (s2 == NULL)
 		s2 = "";
+	new = malloc((sizeof(char) * size));
+	for (iter = 0; s1[iter]; iter++)
+		new[iter] = s1[iter];
 	while (s2[it])
 	{
 		new[iter] = s2[it];
 		iter++;
 		it++;
 	}
-	new[iter] = '\0';
+	new[iter] = 0;
 	return (new);
 }
