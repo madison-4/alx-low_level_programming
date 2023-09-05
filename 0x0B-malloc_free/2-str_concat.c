1762359446df8d74a7d7a629cf5b835115b8158d
nox <samingosi97@gmail.com>
2023-09-05 12:51:52 -0700

    account for empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,68 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * _strlen - get the length of a string
- * @s: string
- * Return: length of the string
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
  */
 int _strlen(char *s)
 {
-	int length = 0, iterator;
+	int a;
 
-	for (iterator = 0; s[iterator] != '\0'; iterator++)
-	{
-		length++;
-	}
-	return (length);
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
 }
 /**
- * str_concat - concatenate two srings in a new place in memory
- * @s1: string 1
- * @s2: string 2
- * Return: newly allocated string pointer
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = _strlen(s1), len2 = _strlen(s2), size = len1 + len2 + 1;
-	int iter = 0, it = 0;
-	char *new;
+	char *ptr;
+	int size1, size2;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	new = malloc((sizeof(char) * size));
-	for (iter = 0; s1[iter]; iter++)
-		new[iter] = s1[iter];
-	while (s2[it])
 	{
-		new[iter] = s2[it];
-		iter++;
-		it++;
+		s1 = "";
 	}
-	new[iter] = 0;
-	return (new);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
