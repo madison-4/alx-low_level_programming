2bf55797c1c22d57a4aa2d01fa641692f8de1ee8
Enox <anzimboingosi@gmail.com>
2023-09-05 04:36:16 +0300

    null check at the start

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -24,9 +24,10 @@ char *_strdup(char *str)
 	char *new;
 	int fill = 1 + _strlen(str), iter;
 
-	if (fill == 0)
-		return (NULL);
+
 	if (str == NULL)
+		return (NULL);
+	if (fill == 0)
 		return (NULL);
 	new = malloc(sizeof(char) * fill);
 	if (new == NULL)
