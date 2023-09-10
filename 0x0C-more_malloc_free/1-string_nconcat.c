ab73604c252bb66822fa3e7a47fbfa4be8eff2a4
nox <samingosi97@gmail.com>
2023-09-10 09:35:35 -0700

    account for null arguments

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,25 @@
 #include "main.h"
 /**
- * string_nconcat - concatenate two strings in a newly allocated space in mempry
+ * string_nconcat - concatenate two strings in a newly allocated space in
  * @s1: first string
  * @s2: second string
  * @n: bytes to contante limit
  * Return: pointer to newly allocated string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = 0, len2 = 0, iter = 0;
 	char *new;
 
+	if (!(s1))
+		s1 = "";
+	if (!(s2))
+		s2 = "";
 	while (s1[len1])
 		len1++;
 	while (s2[len2] && len2 < n)
 		len2++;
-	new = malloc((len1 + len2 + 1) * sizeof (char));
+	new = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!(new))
 		return (NULL);
 	for (iter = 0; iter < len1; iter++)
