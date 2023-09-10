4e4986e0a5a37cfe2266e14b46e5e61372ee5fa6
nox <samingosi97@gmail.com>
2023-09-10 02:13:59 -0700

    use n for strlen(s2) to remove memory leaks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (n >= _strlen(s2))
 		n = _strlen(s2);
-	new = malloc(_strlen(s1) + n + 1);
+	new = malloc(_strlen(s1) + n + 2);
 	if (new == NULL)
 		return (NULL);
 	for (iter = 0; s1[iter]; iter++)
