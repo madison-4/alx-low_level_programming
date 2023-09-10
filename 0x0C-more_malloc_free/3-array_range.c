9b8c3451e51f11dc6d018a017b9f7cf4edbf325d
nox <samingosi97@gmail.com>
2023-09-10 12:21:03 -0700

    use size of an int in malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	array = malloc((max - min) + 1);
+	array = malloc(((max - min) + 1) * sizeof(int));
 	if (!(array))
 		return (NULL);
 	size = (max - min) + 1;
