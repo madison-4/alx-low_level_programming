279a1bd52ed609fbcf038475c71587d8ce12bf81
nox <samingosi97@gmail.com>
2023-09-11 14:09:17 -0700

    use ternary for age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		if (!(d->name))
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		if (!(d->age))
-			printf("Age: %d\n", 0);
+		printf("Age: %f\n", (d->age) ? (d->age) : 0);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		if (!(d->owner))
