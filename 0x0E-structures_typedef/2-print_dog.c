ca634c9b55ed592b723232c75721ecc9cc085cf6
nox <samingosi97@gmail.com>
2023-09-11 13:47:10 -0700

    escape null pointer

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,17 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 	{
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
 	}
 }
