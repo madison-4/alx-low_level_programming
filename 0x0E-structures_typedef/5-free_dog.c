520101bf96c6e66d9ccac9cdce3ba9de3f46cbbc
nox <samingosi97@gmail.com>
2023-09-11 14:13:56 -0700

    create and free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - function to free space
+ * @d: pointer to dog typedef type
+ * Return: Always success
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
