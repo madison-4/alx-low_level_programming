478aba95066167c5bbf82786f1317551c9927192
nox <samingosi97@gmail.com>
2023-09-11 14:03:44 -0700

    create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * new_dog - create a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to newly created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(dog_t));
+	if (!(new))
+		return (NULL);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+
+	return (new);
+}
