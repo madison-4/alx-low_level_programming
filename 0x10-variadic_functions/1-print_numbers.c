d3e1043d4052b50272705e465218b94fccef1bfb
nox <samingosi97@gmail.com>
2023-09-14 16:53:28 -0700

    print numbers and a non-NULL separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - rints numbers
+ * @separator: separatpr to print between nubers
+ * @n: number of numbers
+ * Return: no return, just print and exit
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int iter = 0, num;
+	va_list args;
+
+	va_start(args, n);
+	for (iter = 0; iter < n; iter++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		if (separator && iter < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
