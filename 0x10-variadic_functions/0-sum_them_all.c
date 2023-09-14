d96718724b0c18923c8a5724b02c3d5d718c085e
nox <samingosi97@gmail.com>
2023-09-14 16:31:13 -0700

    sum variale arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * sum_them_all - returns the sum of its numbers arguments
+ * @n: number of numbers to add
+ * Return: sum of the numbers
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, iter, num;
+	va_list args;
+
+	va_start(args, n);
+	for (iter = 0; iter < n; iter++)
+	{
+		num = va_arg(args, int);
+		sum += num;
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+# About
+excercise on variadic functions
