bac2621f18328ee6ae7863811b523fb1eb511d4a
nox <samingosi97@gmail.com>
2023-09-16 01:07:59 -0700

    return index in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an int in an array
+ * @array: array to be searched
+ * @size: size of the array
+ * @cmp: function to compare
+ * Return: index of first ele,ent that matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int iter, index = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (iter = 0; iter < size; iter++)
+	{
+		index = cmp(array[iter]);
+		if (index)
+			return (iter);
+	}
+	return (-1);
+}
