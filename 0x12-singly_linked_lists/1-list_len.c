5c0da666fe5c1a36f9afb9b08e2882eca3fcf990
nox <samingosi97@gmail.com>
2023-09-21 17:36:16 -0700

    get length of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - get the length of a linked lists
+ * @h: address of head
+ * Return: number of elements in a lst
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	list_t *iter = h;
+
+	while (iter)
+	{
+		count++;
+		iter = iter->next;
+	}
+	return (count);
+}
