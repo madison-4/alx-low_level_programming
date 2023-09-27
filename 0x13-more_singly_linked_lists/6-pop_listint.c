ec0adfea34d75d42d5884e5bd3ea07c926a3a19f
nox <samingosi97@gmail.com>
2023-09-27 15:32:34 -0700

    pop head and reset it

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * pop_listint - posps the heaedm resets it and returns its data
+ * @head: address of the head
+ * Return: head's data n
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int rew = temp->n;
+
+	*head = temp->next;
+	free(temp);
+	return (rew);
+}
