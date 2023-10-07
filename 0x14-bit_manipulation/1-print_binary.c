5ba80086ce024cc327fe8a2a9cd436dc7ea829e3
nox <samingosi97@gmail.com>
2023-10-07 11:55:31 +0300

    print bin rep

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print binary rep of number
+ * Return: nio return, it's void
+ */
+void print_binary(unsigned long int n)
+{
+	int bit;
+
+	if (!(n))
+	{
+		_putchar(48);
+		exit(EXIT_SUCCESS);
+	}
+	while (n)
+	{
+		bit = (n >> 1);
+		_putchar(bit + 48);
+	}
+}
