dcd24d79f5ec15df29bc735844253e5582737f4f
nox <samingosi97@gmail.com>
2023-10-09 23:58:54 +0300

    get bitwise and to compare bto 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,10 +7,11 @@
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask, compvalue = 1;
+	unsigned long int mask, compvalue = 1, value;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	mask = (compvalue << index);
-	return (n & mask);
+	value = ((n & mask) != 0);
+	return (value);
 }
