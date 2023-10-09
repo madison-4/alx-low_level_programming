fa179210199be632c1b436e305b74401f0e3b57a
nox <samingosi97@gmail.com>
2023-10-10 01:11:14 +0300

    check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * get_endianness - check endianness]
+  * No arguments
+  * Return: 0 for big, 1 for little
+  */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *c;
+
+	char *c = (char *)&num;
+	return (c[0]);
+}
