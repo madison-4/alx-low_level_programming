eebe43327669291a0721a36960f1f91021056283
nox <samingosi97@gmail.com>
2023-10-10 00:42:20 +0300

    see number of flipped bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * flip_bits - coubnts the bits needed to flip to match a number\
+  * @n: first number
+  * @m: second number
+  * Return: number of bits to flip
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int count = 0, xor_result = n ^ m;
+
+	while (xor_result > 0)
+	{
+		count += (xor_result & 1);
+		xor_result >>= 1;
+	}
+	return (count);
+}
