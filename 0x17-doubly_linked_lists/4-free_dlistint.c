5ae945a4d5931ac7661e8a867e74512ba10d238b
nox <samingosi97@gmail.com>
2023-11-11 02:40:34 -0800

    free a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a doubly linked listsd
+ * @head: head of the list
+ * Return: no return, void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *iter = head;
+
+	if (iter == NULL)
+		return;
+	while (iter->next)
+	{
+		iter = iter->next;
+		free(iter->prev);
+	}
+	free(NULL);
+	free(iter);
+}
