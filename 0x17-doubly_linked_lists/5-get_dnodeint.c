9332cf6bb6a6a2bd765943cf80edf112a15437ac
nox <samingosi97@gmail.com>
2023-11-11 02:53:05 -0800

    return node at a prticular index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == (NULL))
 		return (NULL);
+	if (index < 0)
+		return (NULL);
 	while (i < index)
 	{
 		iter = iter->next;
