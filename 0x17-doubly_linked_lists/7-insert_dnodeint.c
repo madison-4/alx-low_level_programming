20369e784e9c15dde27623403e024aa12a67f004
nox <samingosi97@gmail.com>
2023-11-12 00:23:11 -0800

    make it so that next ptr of prev points to curr

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		iter = iter->next;
 		count++;
 	}
-	if ((count + 1) == idx)
+	if ((count) == idx)
 	{
 		free(new);
 		return (add_dnodeint_end(h, n));
