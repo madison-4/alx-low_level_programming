4d3a1da693a513672a2f665ac029752c95706ff9
nox <samingosi97@gmail.com>
2024-02-04 06:09:40 -0800

    add crete table functions

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,15 +2,18 @@
 /**
  * hash_table_create - function to create a has table in C
  * @size: size of the table
+ * It initializes teh array to NULL
  * Return: pointer to newly created table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(has_table_t));
+	new = malloc(sizeof(hash_table_t));
 	if (!(new))
 		return (NULL);
 	new->size = size;
+	new->array = NULL;
+	free(new);
 	return (new);
 }
