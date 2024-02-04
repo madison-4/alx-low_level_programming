d488879d213e382d278c16b2af76baa5a9f08f6e
nox <samingosi97@gmail.com>
2024-02-04 09:15:19 -0800

    function to return an index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * long int key_index - generate an index from a key
+ * @key: the key to the table, string
+ * @size: size of the hash table
+ * Return: index of the array to use
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, hash;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+	return (index);
+}
