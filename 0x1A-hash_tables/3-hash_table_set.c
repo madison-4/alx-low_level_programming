31072328dca15a245e442ebc75aa46766da280ec
nox <samingosi97@gmail.com>
2024-02-05 16:20:44 -0800

    insertion function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#include "hash_tbles.h"
+#include "hash_tables.h"
 /**
  * hash_table_set - a function that adds an element to te hash table
  * @ht: address of the given hash table
@@ -8,15 +8,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item;
-	unigned long int hash, index;
+	hash_node_t *item, *temp, *prev, *comp;
+	char *key1, *value_copy;
+	unsigned long int index;
 
-	item = malloc(sizeof(hash_node_t));
-	if (!(item))
+	if ((ht == NULL) || (key == NULL) || (*key == '\0') || (value == NULL))
 		return (0);
-	item->key = key;
-	item->value = value;
+	item = malloc(sizeof(hash_node_t));
+	value_copy = strdup(value);
+	key1 = strdup(key);
+	if ((value_copy == NULL) || (key1 == NULL) || (item == NULL))
+	{
+		free(item);
+		return (0);
+	}
+	item->key = key1;
+	item->value = value_copy;
 	item->next = NULL;
-	hash = hash_djb2(key);
-	index = 
+	index = key_index((const unsigned char *)item->key, ht->size);
+	if (ht->array[index] == NULL)
+		ht->array[index] = item;
+	else if (ht->array[index] != NULL)
+	{
+		comp = ht->array[index];
+		if (strcmp((comp->key), key) == 0)
+			comp->value = value_copy;
+		else
+		{
+			temp = ht->array[index];
+			while (temp)
+			{
+				prev = temp;
+				temp = temp->next;
+			}
+			prev->next = item;
+			return (1);
+		}
+	}
+	return (1);
 }
