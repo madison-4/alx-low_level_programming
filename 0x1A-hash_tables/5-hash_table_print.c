3966f5f07becaaf4bb7297bc7981e9de6118fc4f
nox <samingosi97@gmail.com>
2024-02-06 16:16:03 -0800

    get comma printing after lists

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,22 +8,29 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *item;
+	char list_flag = 0;
 
 	if (!(ht))
+	{
+		puts("{}");
 		return;
+	}
 	putchar('{');
 	for (i = 0; i < ht->size; i++)
 	{
 		item = ht->array[i];
 		if (item == NULL)
 			continue;
+		if (list_flag == 1)
+			printf(", ");
 		while (item)
 		{
-			printf("\'%s\' : \'%s\'", item->key, item->value);
+			printf("\'%s\': \'%s\'", item->key, item->value);
 			item = item->next;
+			if (item)
+				printf(", ");
 		}
-		if (i < (ht->size - 1))
-			putchar(',');
+		list_flag = 1;
 	}
 	putchar('}');
 	putchar('\n');
