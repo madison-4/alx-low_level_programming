f3931b79312cca957c54619461874cd82d10bf81
nox <samingosi97@gmail.com>
2024-02-08 11:59:19 -0800

    add table struct

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -7,8 +7,21 @@
 shash_table_t *shash_table_create(unsigned long int size)
 {
 	shash_table_t *table;
+	unsigned long int i;
 
-	table = malloc(shash_table_t);
+	table = malloc(sizeof(shash_table_t));
 	if (!(table))
 		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(shash_node_t *) * size);
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	table->shead = NULL;
+	table->stail = NULL;
+	return (table);
 }
