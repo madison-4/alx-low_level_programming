55a5eb055a01faba3e8fb737e6570fe89a8b0f95
nox <samingosi97@gmail.com>
2024-02-08 11:19:59 -0800

    add initial function

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,3 +1,14 @@
 #include "hash_tables.h"
-	/**
-	 * 
+/**
+ * shash_table_create - function to create a hash table
+ * @size: size of the hash table
+ * Return: pointer to new table or NULL on failure
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *table;
+
+	table = malloc(shash_table_t);
+	if (!(table))
+		return (NULL);
+}
